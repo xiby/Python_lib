bb80c86a3b6802bf1b5d590aff66b2da6852e6f0
Xiby John <Xiby John>
2017-05-04 14:28:30 +0800

    服务器已经搭建成功，再向其中加入一些功能，就可进入界面搭建的环节。

--- pySC/server.py
+++ pySC/server.py
@@ -1,5 +1,5 @@
 from socket import *
-from time import ctime
+import time
 import dataBase
 
 HOST=''
@@ -27,6 +27,14 @@ while True:
             book=list()
             book.append(data[1])
             book.append(data[2])
+            now=time.strftime("%Y %m %d %X",time.localtime())
+            now=now.split(" ")
+            book.append(now[0])
+            book.append(now[1])
+            book.append(now[2])
+            
+            book.append(1)
+            print(book)
             myLib.insert(book)
         elif data[0]=='2':              #表明此时要查询全部数据
             print(myLib.search())
